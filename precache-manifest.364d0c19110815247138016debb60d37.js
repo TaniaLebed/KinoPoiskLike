1e53698cb7f554d9a4adb9b19e888a56d03e75eb
TaniaLebed <lebedtania666@gmail.com>
2020-09-14 10:52:05 +0300

    Updates

---
+++ precache-manifest.364d0c19110815247138016debb60d37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b2963b002ef83535057b50c9613087",
+    "url": "/kinopoisk-like/index.html"
+  },
+  {
+    "revision": "2d36e693e3161af4ddae",
+    "url": "/kinopoisk-like/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6cf8692b40da9d67c183",
+    "url": "/kinopoisk-like/static/css/main.99690089.chunk.css"
+  },
+  {
+    "revision": "2d36e693e3161af4ddae",
+    "url": "/kinopoisk-like/static/js/2.71bfd5a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kinopoisk-like/static/js/2.71bfd5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf8692b40da9d67c183",
+    "url": "/kinopoisk-like/static/js/main.952f7b92.chunk.js"
+  },
+  {
+    "revision": "e94a8a7e56d8adf9283a",
+    "url": "/kinopoisk-like/static/js/runtime-main.0988e008.js"
+  }
+]);
